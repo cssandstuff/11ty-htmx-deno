7f9391cf97d3af48379adca57d91f975baf14cf2
Tim Clulow <tim@cssandstuff.com>
2024-07-21 10:32:48 +0900

    more simple server setup

--- server/deps.ts
+++
@@ -1,3 +0,0 @@
-// Main dependencies for Deno all listed in one file
-export { Application, Context, Router, Status, send } from "https://deno.land/x/oak@14.2.0/mod.ts";
-export { staticFileRoutes } from "../server/staticFileRoutes.ts";
